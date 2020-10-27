112e50a613a10a4e85ba3a2e86e8eeb23e655726
yxb <xiangbo_x@qq.com>
2020-10-27 16:12:24 +0800

    kousuan test

--- kousuan.py
+++ kousuan.py
@@ -2,26 +2,29 @@ import random
 
 pcount=100
 maxnum=10
-for i in range(1,pcount):
-   a=random.randint(0,maxnum)
-   b=random.randint(0,maxnum)
-   c=max(a+b,maxnum)
-   d=random.randint(0,65535)%2==0
-   if (d):
-      op='-'
-   else:
-      op='+'
-   e=random.randint(0,3)
-   if (e==0):
-       print('(   )'+op+a+'='+c
-   elif (e==1):
-       print(c+op+'(   )='+a
-   else:
-       print(c+op+a+'=(   )'
-   if (i%5==0):
-       print('\r\n')
-   else:
-       print('    ')
+for i in range(0,pcount):
+    a=random.randint(0,maxnum)
+    b=random.randint(0,maxnum)
+    c=max(a+b,maxnum)
+    d=random.randint(0,65535)%2==0
+    if d:
+        op='-'
+    else:
+        op='+'
+    e=random.randint(0,3)
 
+    if e==0:
+        print('(   ) %s %d = %d' % (op, a , c), end="    ")
+    elif e==1:
+        if d:
+            print('%d %s (   ) = %d' % (c, op, a), end="    ")
+        else:
+            print('%d %s (   ) = %d' % (a, op, c), end="    ")
+    else:
+        if d:
+            print('%d %s %d = (   )' % (c, op, a), end="    ")
+        else:
+            print('%d %s %d = (   )' % (a, op, b), end="    ")
 
-
+    if (i+1)%5==0:
+        print('\r\n')
